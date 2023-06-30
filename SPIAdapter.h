b06a098fdac1164661feddf36c7501647af9b85d
MatteoSchmider <matteoschmider@aol.com>
2023-06-30 02:29:52 +0200

    make virtual class work

--- SPIAdapter.h
+++ SPIAdapter.h
@@ -4,13 +4,13 @@
 
 class SPIAdapter {
 public:
-  virtual ~SPIAdapter(){};
+  virtual ~SPIAdapter() {}
 
-  virtual uint32_t readTMC4671(uint8_t address) const;
+  virtual uint32_t readTMC4671(uint8_t address) const {};
 
-  virtual void writeTMC4671(uint8_t address, uint32_t data) const;
+  virtual void writeTMC4671(uint8_t address, uint32_t data) const {};
 
-  virtual void writeTMC6100(uint8_t address, uint32_t data) const;
+  virtual void writeTMC6100(uint8_t address, uint32_t data) const {};
 
-  virtual uint16_t readAS5047P(uint16_t address) const;
+  virtual uint16_t readAS5047P(uint16_t address) const {};
 };
